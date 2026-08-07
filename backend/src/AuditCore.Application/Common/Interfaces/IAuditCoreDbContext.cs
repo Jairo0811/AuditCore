740edfc57ecbfa5c16e6d46cc64d94bdf33fe259
Jairo Matías <jairo_1829@hotmail.com>
2026-08-07 13:43:34 -0400

    feat: expose audit execution sets

--- backend/src/AuditCore.Application/Common/Interfaces/IAuditCoreDbContext.cs
+++ backend/src/AuditCore.Application/Common/Interfaces/IAuditCoreDbContext.cs
@@ -1,4 +1,4 @@
-﻿using AuditCore.Domain.Entities;
+using AuditCore.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuditCore.Application.Common.Interfaces;
@@ -6,29 +6,22 @@ namespace AuditCore.Application.Common.Interfaces;
 public interface IAuditCoreDbContext
 {
     DbSet<Organization> Organizations { get; }
-
     DbSet<Branch> Branches { get; }
-
     DbSet<Department> Departments { get; }
-
     DbSet<Audit> Audits { get; }
-
     DbSet<Risk> Risks { get; }
-
     DbSet<Finding> Findings { get; }
-
+    DbSet<Evidence> Evidences { get; }
+    DbSet<ActionPlan> ActionPlans { get; }
+    DbSet<ControlFramework> ControlFrameworks { get; }
+    DbSet<ControlDefinition> ControlDefinitions { get; }
+    DbSet<ControlEvaluation> ControlEvaluations { get; }
     DbSet<User> Users { get; }
-
     DbSet<Role> Roles { get; }
-
     DbSet<Permission> Permissions { get; }
-
     DbSet<UserRole> UserRoles { get; }
-
     DbSet<RolePermission> RolePermissions { get; }
-
     DbSet<RefreshToken> RefreshTokens { get; }
 
-    Task<int> SaveChangesAsync(
-        CancellationToken cancellationToken = default);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
